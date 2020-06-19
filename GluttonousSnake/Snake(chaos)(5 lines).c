c7309b889602ec08baee41321a3be82283167eaf
RainbowRoad1 <58882244+RainbowRoad1@users.noreply.github.com>
2020-06-19 17:20:45 +0800

    Create Snake(chaos)(5 lines).c

---
+++ GluttonousSnake/Snake(chaos)(5 lines).c
@@ -0,0 +1,14 @@
+main(){int W=15,S=W*W,m[S],z[2]={0},l=3,c='L',C,i=S,*p=m;while(i--)m[i]=0;srand
+(time(0));*p=l;f:for(++l;m[i=rand()%S];);for(m[i]--;C-80;_sleep(150)){for(*p=l,
+i=system("cls");i<S;++i%W||puts("|"))printf(m[i]>0&&m[i]--?"[]":m[i]?"00":"  ")
+;kbhit()&&(C=getch()&95,C-1>>2^18||(c^C)&3^2&&(c=C));p=z+c%2,*p-=~-c&2^2,*p=(++
+*p+W)%W;p=&m[*z+z[1]*W];if(*p<0)goto f;if(*p)break;}}
+
+/*
+使用gcc编译通过(MinGW)(gcc version 8.2.0)
+没想到吧，又压缩了很多...总共374字符,修改&增加了很多细节
+1.ijkl控制方向(ijkl更方便位运算...至少比wasd方便)
+2.不修改窗口大小,直接用换行代替了,如果窗口不够手动拉伸下
+3.增加了p键退出(实现这个就加了3个字符,可以改任意键位)
+4.更方便修改地图大小,改W的值就行了(但长宽比固定1:1)
+*/
