d2752aef88c469296ce13d8bd62c34b29139a34e
RainbowRoad1 <58882244+RainbowRoad1@users.noreply.github.com>
2020-07-04 21:10:44 +0800

    Update MineSweeper(keyboard).c

--- MineSweeper/MineSweeper(keyboard).c
+++ MineSweeper/MineSweeper(keyboard).c
@@ -1,30 +1,31 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
-#define OUT (x < 0 || 9 <= x || y < 0 || 9 <= y)
-int map[81] = {0}, mark[81] = {0}, count = 81, z, c, i, j, s, (*f)(int, int);
-void tmp(int x, int y) { OUT || map[x + 9 * y] - 16 && map[x + 9 * y]++; }
-void dig(int x, int y){
-	if (OUT || mark[i = x + y * 9] || (mark[i]++, count--, map[i]))return;
+#define OUT (x < 0 || W <= x || y < 0 || H <= y)
+int W = 9, H = 9, B = 10, *m, *k, s, z, c = 1, i, j, (*f)(int, int);
+void tmp(int x, int y) { OUT || m[x + W * y] - 16 && m[x + W * y]++; }
+void dig(int x, int y) {
+	if (OUT || k[i = x + W * y] || (k[i]++, s--, m[i]))return;
 	for (int i = 0; i < 9; ++i - 4 || i++)f(x + i % 3 - 1, y + i / 3 - 1);
 }
-void set(int x, int y){
-	for (srand(time(0)); c++ < s; dig(j % 9, j / 9), map[j] = 16, mark[j]--)
-		for (f = tmp; map[j = rand() % 81] > 9 || z == j || (map[j] = 0););
-	f = dig, count += s, dig(x, y);
+void set(int x, int y) {
+	for (srand(m), f = tmp; c++ < B; dig(j % W, j / W), m[j] = 16, k[j]--)
+		for (; m[j = rand() % (W * H)] > 9 || z == j || (m[j] = 0););
+	f = dig, s += B, dig(x, y);
 }
-int main(){
-	for (f = set, c = 1, z = 40 ,s = 10; c - 27;c = getch() & 95){
-		c - 68 || z++, c - 65 || z--, c - 83 || (z += 9), c - 87 || (z -= 9);
-		z = (z + 81) % 81, c || (f(z % 9, z / 9), 0), system("cls");
-		for (i = 0; j = i - z ? 32 : 62, i < 81; 1, ++i % 9 || _cputs("\n"))
-			_cprintf("%c%c", j, mark[i] ? map[i] ? 48 + map[i] : 32 : 42);
-		if((j = count <= 10) || c == 0 && map[z] > 9)break;
+int main() {
+	for (f = set, s = W * H, m = calloc(s * 2, 4), k = m + s; c - 27; j = 0) {
+		c - 68 || z++, c - 65 || z--, c - 83 || (z += W), c - 87 || (z -= W);
+		z = (z + W * H) % (W * H), c || (f(z % W, z / W), 0), system("cls");
+		for (i = 0; j = i - z ? 32 : 62, i < W * H; ++i % W || _cputs("\n"))
+			_cprintf("%c%c", j, k[i] ? m[i] ? 48 + m[i] : 32 : 42);
+		if (!(j = c || m[z] < 9) || s <= B || (c = _getch() & 95, 0))break;
 	}
-	_cprintf("%s! %ds", j ? "you win" : "Game over", clock() / 1000), getch();
+	_cprintf("%s! %ds", j ? "you win" : "Game over", clock() / 1000), _getch();
 }
 
 /*
+修改了一些细节，可以更方便地修改地图大小了(别设置太大了,会栈溢出),行数没变就直接覆盖原来的代码了(2020.7.4)
 使用gcc编译通过(MinGW)(gcc version 8.2.0)
 不仅减少了很多代码,交互也比上一次的更好了
 wasd键控制光标,空格键挖开当前格子,Esc键退出
