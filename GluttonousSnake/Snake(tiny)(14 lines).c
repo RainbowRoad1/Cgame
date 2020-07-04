8867cd2ed8e4c1407487c1c0c6fd6ceddcd45c6c
RainbowRoad1 <58882244+RainbowRoad1@users.noreply.github.com>
2020-07-04 17:08:05 +0800

    Update Snake(tiny)(14 lines).c

--- GluttonousSnake/Snake(tiny)(14 lines).c
+++ GluttonousSnake/Snake(tiny)(14 lines).c
@@ -1,15 +1,15 @@
 #include <stdlib.h>
 #include <conio.h>
 int main() {
-	int W = 20, S = W * W, *map, z[3] = { 0 }, len = 3, i, c = 'D', C, *p;
+	int W = 20, S = W * W, *map, z[3] = { 0 }, len = 3, i, c = 'D', C, *p, a;
 	for (srand(map = calloc(S, 4)), C = map[1] = -1; C - 27; _sleep(100)) {
 		if (_kbhit())C = _getch() & 95, C - 65 && C - 68 &&
 			C - 83 && C - 87 || ((C ^ c) & 20) - 4 && (c = C);
 		p = z + !!(c & 16), *p += c / 6 & 1 ? 1 : -1, *p = (*p + W) % W;
-		if (C = 1, map[z[2] = *z + z[1] * W] > 1 || system("cls"))break;
-		for (; map[z[2]] && (map[i = rand() % S] || (--map[i], ++len, --C)););
+		if (a = 1, map[z[2] = *z + z[1] * W] > 0 || system("cls"))break;
+		for (; map[z[2]] && (map[i = rand() % S] || (--map[i], ++len, --a)););
 		for (map[z[2]] = len, i = 0; i < S; ++i % W || _cputs("|\n"))
-			_cputs(map[i] > 0 && (map[i] -= C) ? "()" : map[i] ? "00" : "  ");
+			_cputs(map[i] > 0  ? ((map[i] -= a), "()") : map[i] ? "00" : "  ");
 	}
 }
 
