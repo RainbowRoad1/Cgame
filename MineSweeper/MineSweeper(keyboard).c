acb97e1997f61d4bec35e093a33412c5f9de8494
RainbowRoad1 <58882244+RainbowRoad1@users.noreply.github.com>
2020-10-03 16:20:55 +0800

    Add comments

--- MineSweeper/MineSweeper(keyboard).c
+++ MineSweeper/MineSweeper(keyboard).c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <conio.h>
-#include <time.h>
 #define OUT (x < 0 || W <= x || y < 0 || H <= y)
 int W = 9, H = 9, B = 10, *m, *k, s, z, c = 1, i, j, (*f)(int, int);
 void tmp(int x, int y) { OUT || m[x + W * y] - 16 && m[x + W * y]++; }
@@ -21,10 +20,18 @@ int main() {
 			_cprintf("%c%c", j, k[i] ? m[i] ? 48 + m[i] : 32 : 42);
 		if (!(j = c || m[z] < 9) || s <= B || (c = _getch() & 95, 0))break;
 	}
-	_cprintf("%s! %ds", j ? "you win" : "Game over", clock() / 1000), _getch();
+	_cputs(j ? "You win!" : "Game over!"), _getch();
 }
 
 /*
+24行扫雷, 1088字符, 实现了简单的功能;
+ADSW移动光标, 空格挖开对应格子, 剩余格子等于雷的数量或挖到雷或按下Esc键时游戏结束;
+可自定义地图尺寸(W, H), 雷的数量(B)
+于2020.6.25上传, 2020.10.3补充说明并删除部分代码
+删除: 结束时显示运行时间 (不影响游戏主体, 行数-1)
+
+下面是之前的说明:
+
 修改了一些细节，可以更方便地修改地图大小了(别设置太大了,会栈溢出),行数没变就直接覆盖原来的代码了(2020.7.4)
 使用gcc编译通过(MinGW)(gcc version 8.2.0)
 不仅减少了很多代码,交互也比上一次的更好了
