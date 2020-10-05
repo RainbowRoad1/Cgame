677895e4ae041ebe18f6d2851c5a36146925abb0
RainbowRoad1 <58882244+RainbowRoad1@users.noreply.github.com>
2020-10-05 16:31:57 +0800

    Add comments and renamed

--- Five-in-a-row/1_tiny_19lines.c
+++ Five-in-a-row/1_tiny_19lines.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include <conio.h>
 int W = 13, S, *m, z = 0, c = 1, r = 2, i, j;
-int sum(int v, int l, int s){
-    return s && (abs(v % W - (v + l) % W) - W + 1 && v + l >= 0 && v + l < S)
-        && m[v] == m[v + l] ? 1 + sum(v + l, l, s - 1) : 0;
+int sum(int v, int l){
+    return (abs(v % W - (v + l) % W) - W + 1 && v + l >= 0 && v + l < S) && 
+        m[v] == m[v + l] ? 1 + sum(v + l, l) : 0;
 }
 int main(){
     for (m = calloc(S = W * W, 4); r < 4 && c - 27; c = _getch() & 95){
@@ -12,17 +12,22 @@ int main(){
             for (m[z] = r ^= 3; j = i % 3 - 1 + i / 3 * W, i < 6; ++i)
                 sum(z, j, 4) + sum(z, -j, 4) > 3 && (r ^= 3, i = r += 4);
         for (i = 0; i < S; ++i % W || _cprintf("%d\n", i / W))
-            _cprintf("%c%c", i - z ? 32 : 62, m[i] ? m[i] - 1 ? 64 : 79 : 32);
+            _cprintf("%c%c", " >"[i == z], " 0@"[m[i]]);
         for (i = 0; i < W; ++i)_cprintf(" %c", 97 + i);
-        _cprintf("\n%s", r & 1 ? "White" : "Black"), r < 4 || _cputs(" win!");
+        _cprintf(r & 1 ? "\nWhite" : "\nBlack"), r < 4 || _cputs(" win!");
     }
 }
 
 /*
 adsw移动，空格下子，Esc退出，黑方先手，先达成五连的获胜
-'@'是白棋,'O'是黑棋(对应15行的64和79,可以自己改)
+'@'是白棋,'O'是黑棋,'>'是光标, 可自定义棋盘尺寸(W)
 大小可以自己改(3行的W)(固定长宽比1:1)
 和上次的迷宫一样，感觉没什么很复杂的部分，界面和交互直接照搬扫雷
-*感谢某热心群友测试指出一处隐患和一处逻辑错误
-*真有群友测试是我万万没想到的...
+
+感谢某热心群友测试指出一处隐患和一处逻辑错误(2020.7.25更新)
+真有群友测试是我万万没想到的...
+
+2020.10.5优化部分代码
+
+于2020.7.24上传
 */
--- Maze/1_tiny_26lines.c
+++ Maze/1_tiny_26lines.c
@@ -14,13 +14,23 @@ void dig(int v) {
 }
 int main() {
 	srand((unsigned)malloc(!system("mode con:cols=42 lines=21")));
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &(CONSOLE_CURSOR_INFO) { 25, 0 });
+	SetConsoleCursorInfo(GetStdHandle((DWORD)-11), &(CONSOLE_CURSOR_INFO){25});
 	dig(Long + 1), map[Long] = map[Long*Long - Long - 1] = 1;
 	for (c = 0; c = _getch(), place - Long * Long + Long + 1;) {
 		for (i = 0; c - move[i + 4] && i < 4; i++);
 		i < 4 && map[place + move[i]] && (place += move[i]);
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), (COORD) { 0, 0 });
+		SetConsoleCursorPosition(GetStdHandle((DWORD)-11), (COORD){0});
 		for (i = 0; i < Long*Long; i++)
 			_cputs(i - place ? map[i] ? "  " : "[]" : "<>");
 	}
 }
+
+/*
+26行走迷宫, 1110字符, 实现了简单的功能;
+adsw移动(不支持大写), 直到移动到右下角时游戏结束;
+可自定义地图尺寸(Long)
+这个算是最水的一个作品了...实现的效果并不是很好
+目前打算重写一遍, 让自动生成的迷宫更加自然
+由于去掉闪烁多了两行代码, 和对应视频内容有一点出入
+于2020.4.24上传, 2020.10.3补充说明并修改部分代码
+*/
