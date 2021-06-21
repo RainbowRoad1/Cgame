68ed15fe7255060a97915397a6eb4af551a0cec2
kaiyuanX <71761693+kaiyuanX@users.noreply.github.com>
2021-06-21 23:41:34 +0800

    转移sleep的位置
    把sleep放在打印语句的后面，kbhit( )的前面。  
    sleep之后再进行kbhit( )的判断，游戏会更加流程。

--- GluttonousSnake/2_chaos_5lines.c
+++ GluttonousSnake/2_chaos_5lines.c
@@ -1,6 +1,6 @@
 main(){int W=15,S=W*W,*m=calloc(S,4),z[2]={0},l=3,c=76,C,i,*p=m;srand(m);f:for(
-++l;m[i=rand()%S];);for(m[i]--;C-80;_sleep(99)){for(i=system("cls"),*p=l;i<S;++
-i%W||puts("|"))printf(m[i]>0&&m[i]--?"[]":m[i]?"00":"  ");if(kbhit())C=getch()&
+++l;m[i=rand()%S];);for(m[i]--;C-80;){for(i=system("cls"),*p=l;i<S;++
+i%W||puts("|"))printf(m[i]>0&&m[i]--?"[]":m[i]?"00":"  ");_sleep(99);if(kbhit())C=getch()&
 95,C-1>>2^18||(c^C)&3^2&&(c=C);p=z+c%2,*p+=~-c&2,*p=(--*p+W)%W;p=m+*z+z[1]*W;if
 (*p<0)goto f;if(*p)break;}}
 
